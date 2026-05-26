8fff777960ca0a2bf522235cdaf6b6b564dec7be
Dennis Braun <169400960+itsDNNS@users.noreply.github.com>
2026-05-26 23:16:00 +0200

    Refine Home signal-family KPI cards (#532)

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 'v31';
+var CACHE_VERSION = 'v32';
 var SHELL_CACHE = 'docsight-shell-' + CACHE_VERSION;
 var STATIC_CACHE = 'docsight-static-' + CACHE_VERSION;
 var OFFLINE_SHELL_HEADERS = {
