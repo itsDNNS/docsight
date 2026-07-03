8ed6e43354ad8ad09e4d589e14632db34adf4ff8
Dennis <169400960+itsDNNS@users.noreply.github.com>
2026-07-03 23:27:47 +0200

    Improve settings layout at tablet widths

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 'v63';
+var CACHE_VERSION = 'v64';
 var SHELL_CACHE = 'docsight-shell-' + CACHE_VERSION;
 var STATIC_CACHE = 'docsight-static-' + CACHE_VERSION;
 var OFFLINE_SHELL_HEADERS = {
